6e934d9cbcd2e2e4d8c5a70070ede4b6d30588f4
Reckordp <rmuhammad134@gmail.com>
2019-03-15 15:57:55 +0700

    OKREK

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-ENV['LINE_CHANNEL_SECRET'] = '66a18397b274e6c8ef5c6bf9ef5c3c69'
+ENV['LINE_CHANNEL_SECRET'] = '38e6f9d072e1cdd0fa2f52bf06afacec'
 ENV['LINE_CHANNEL_TOKEN'] = 'vuuCzI8d5uwPulk0gyHFWg0q+gXDGVcdXj+HMky+MX/JLWtL6CXxwmzxL0WvPspgeHKUmmrNr3DLdAAkwjkvi3qNRQzT5BgkuOezj73GWbhJfKIOsV1bKKU+HvVDu/DkZq2vi6FtZAaQiPMW4Pvr2gdB04t89/1O/w1cDnyilFU='
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
